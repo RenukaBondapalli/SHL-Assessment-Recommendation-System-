c1f2713f99074320bc6d8b1190fcac3af53cdbad
Renuka Bondapalli <159697639+RenukaBondapalli@users.noreply.github.com>
2025-04-07 09:03:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,5 +133,9 @@ def recommendation_api():
     structured_data = markdown_table_to_json(raw_markdown)
     return jsonify(structured_data)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Use PORT from Render or fallback to 5000
+    app.run(host='0.0.0.0', port=port) 
